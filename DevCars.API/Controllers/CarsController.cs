cdd9f0cf282010f496aa2badb8ddc6064b44cf17
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-03-31 21:10:36 -0300

    Add project files.

---
+++ DevCars.API/Controllers/CarsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCars.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ DevCars.API/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCars.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult PostOrder(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}/orders/{ordersId}")]
+        public IActionResult GetOrder(int id, int ordersId)
+        {
+            return Ok();
+        }
+    }
+}
